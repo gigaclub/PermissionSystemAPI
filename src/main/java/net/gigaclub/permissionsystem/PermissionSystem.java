f82457d20ef45e8f9c72e5e9a4ec24177805b9da
Kevin <xxkevtvxx@gmail.com>
2022-05-25 20:38:55 +0200

    initial commit

---
+++ src/main/java/net/gigaclub/permissionsystem/PermissionSystem.java
@@ -0,0 +1,10 @@
+package net.gigaclub.permissionsystem;
+
+import net.gigaclub.base.odoo.Odoo;
+import org.apache.xmlrpc.XmlRpcException;
+
+import java.util.*;
+
+public class PermissionSystem {
+
+}

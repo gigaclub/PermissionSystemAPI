933f4d9bddd786301f568caf33c212b2e89b859f
Kevin <xxkevtvxx@gmail.com>
2022-05-25 20:49:33 +0200

    start

--- src/main/java/net/gigaclub/permissionsystem/PermissionSystem.java
+++ src/main/java/net/gigaclub/permissionsystem/PermissionSystem.java
@@ -7,4 +7,10 @@ import java.util.*;
 
 public class PermissionSystem {
 
+    private Odoo odoo;
+
+    public PermissionSystem(String hostname, String database, String username, String password) {
+        this.odoo = new Odoo(hostname, database, username, password);
+    }
+
 }

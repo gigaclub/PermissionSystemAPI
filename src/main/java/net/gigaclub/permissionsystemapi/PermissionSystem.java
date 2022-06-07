95a2f58ccba3555544f4b28c61ba6a53f7883318
Kevin <kevin@fritsch.vip>
2022-06-07 06:45:20 +0200

    added new methods

--- src/main/java/net/gigaclub/permissionsystemapi/PermissionSystem.java
+++ src/main/java/net/gigaclub/permissionsystemapi/PermissionSystem.java
@@ -3,6 +3,7 @@ package net.gigaclub.permissionsystemapi;
 import net.gigaclub.base.odoo.Odoo;
 import org.apache.xmlrpc.XmlRpcException;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.*;
 
@@ -26,6 +27,51 @@ public class PermissionSystem {
                         "gc.permission.group",
                         "get_all_groups",
                         Arrays.asList()
+                    )
+                )
+            );
+        } catch (XmlRpcException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public int setGroups(String playerUUID, int[] groupIds) {
+        try {
+            return (int) this.odoo.getModels().execute("execute_kw", Arrays.asList(
+                    this.odoo.getDatabase(), this.odoo.getUid(), this.odoo.getPassword(),
+                    "gc.permission.group", "set_groups", Arrays.asList(playerUUID, groupIds)
+            ));
+        } catch (XmlRpcException e) {
+            e.printStackTrace();
+        }
+        return 1;
+    }
+
+    public int removeGroups(String playerUUID, int[] groupIds) {
+        try {
+            return (int) this.odoo.getModels().execute("execute_kw", Arrays.asList(
+                    this.odoo.getDatabase(), this.odoo.getUid(), this.odoo.getPassword(),
+                    "gc.permission.group", "remove_groups", Arrays.asList(playerUUID, groupIds)
+            ));
+        } catch (XmlRpcException e) {
+            e.printStackTrace();
+        }
+        return 4;
+    }
+
+    public JSONArray getGroups(String playerUUID) {
+        try {
+            return new JSONArray(
+                this.odoo.getModels().execute(
+                    "execute_kw",
+                    Arrays.asList(
+                        this.odoo.getDatabase(),
+                        this.odoo.getUid(),
+                        this.odoo.getPassword(),
+                        "gc.permission.group",
+                        "get_groups",
+                        Arrays.asList(playerUUID)
                     )
                 )
             );
